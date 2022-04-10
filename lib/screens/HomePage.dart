790219eb296e46cf25c62a55c1c7afb0dccdd848
suleiman701 <s125388@student.squ.edu.om>
2022-04-10 22:17:50 +0400

    adding login_screen and image for background

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 
 
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       backgroundColor: Colors.amberAccent,
       appBar: AppBar(),
+      drawer: Drawer(
+
+      ),
 
     );
   }
--- lib/screens/login_signin_screen/login_screen.dart
+++ lib/screens/login_signin_screen/login_screen.dart
@@ -11,7 +11,7 @@ class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      resizeToAvoidBottomInset: false,
 
       body: Center(
 
