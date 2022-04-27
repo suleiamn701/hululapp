873283ffff2e6521027bda88e016507c079651fb
HMawali <almaawali.2014@gmail.com>
2022-04-27 05:09:49 +0400

    adding login rout to register page

--- lib/screens/login_signin_screen/register_screen.dart
+++ lib/screens/login_signin_screen/register_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 
 import 'login_screen.dart';
 
-class  Register extends StatelessWidget {
+class Register extends StatelessWidget {
   //const ({Key? key}) : super(key: key);
   TextEditingController username = TextEditingController();
   TextEditingController fullname = TextEditingController();
@@ -15,182 +15,185 @@ class  Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    resizeToAvoidBottomInset: false,
-
-    body: Center(
-
-    child: Container(
-        constraints: BoxConstraints.expand(),
-        decoration: const BoxDecoration(
-        image: DecorationImage(
-        image: AssetImage("assets/images/backg.png"),
-        fit: BoxFit.cover),
-        ),
-
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-
-        children: [
-
-          // ##############  Register container   ##############
-
-          Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(15),
-              child: const Text(
-                'Register',
-                style: TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold),
-              )),
-
-          // ##############  First name Textfiled   ##############
-
-          Container(
-            padding: const EdgeInsets.all(15),
-            child: TextField(
-
-              controller: username,
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-
-                prefixIcon: Icon(Icons.person,color: Colors.white60,),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color:  Colors.white60,width: 3)),
-
-                labelText: ' Username',
-                  labelStyle: TextStyle(
-                    color:  Colors.white60,
-                  )
-
-              ),
-
-            ),
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/backg.png"),
+                fit: BoxFit.cover),
           ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // ##############  Register container   ##############
 
-          // ##############  last name Textfiled   ##############
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(15),
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  )),
 
-          Container(
-            padding: const EdgeInsets.all(15),
-            child: TextField(
+              // ##############  First name Textfiled   ##############
 
-              controller: fullname,
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-
-                prefixIcon: Icon(Icons.person,color: Colors.white60,),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color:  Colors.white60,width: 3)),
-
-                labelText: ' full Name',
-
-                  labelStyle: TextStyle(
-                    color:  Colors.white60,
-                  )
-
-              ),
-
-            ),
-          ),
-
-          // ##############  Email Address Textfiled   ##############
-
-          Container(
-            padding: const EdgeInsets.all(15),
-            child: TextField(
-
-              controller: email,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-
-                prefixIcon: Icon(Icons.email,color: Colors.white60,),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color:  Colors.white60,width: 3)),
-
-                labelText: ' Email Address',
-                  labelStyle: TextStyle(
-                    color:  Colors.white60,
-                  )
-
-              ),
-
-            ),
-          ),
-
-          // ##############  Password Textfiled   ##############
-
-          Container(
-            padding: const EdgeInsets.all(15),
-            child: TextField(
-
-              controller: password,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: const InputDecoration(
-
-                prefixIcon: Icon(Icons.lock,color: Colors.white60,),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color:  Colors.white60,width: 3)),
-
-                labelText: ' Password',
-                  labelStyle: TextStyle(
-                    color:  Colors.white60,
-                  )
-
-              ),
-
-            ),
-          ),
-
-          // ############## Register Button  ##############
-
-          Container(
-              height: 50,
-              margin: EdgeInsets.only(top: 20),
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.green,
+              Container(
+                padding: const EdgeInsets.all(15),
+                child: TextField(
+                  controller: username,
+                  keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.white60,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white60, width: 3)),
+                      labelText: ' Username',
+                      labelStyle: TextStyle(
+                        color: Colors.white60,
+                      )),
                 ),
+              ),
 
-                child: const Text('Register'),
-                onPressed: () async{
+              // ##############  last name Textfiled   ##############
 
-           try{
-             var authresults = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                 email: email.text.trim(), password: password.text.trim());
-             FirebaseFirestore.instance
-                 .collection('users')
-                 .doc(authresults.user?.uid)
-                 .set({
-               'username': username.text.trim(),
-               'fullname': fullname.text.trim(),
-               'email': email.text.trim(),
-             }).then((value) =>
-                 Navigator.push(context,
-                     MaterialPageRoute(builder: (context) => SignIn()))
-             );
-           } on PlatformException catch (err) {
-             var message = 'An error occurred, Please check your credentionals';
-             if (err.message != null) {
-               message = err.message!;
-             }
-             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-               content: Text(message,style: TextStyle(color: Colors.white),),
-               backgroundColor: Colors.green,
-               behavior: SnackBarBehavior.floating,
-               action: SnackBarAction(label: 'Dismiss',onPressed: (){},textColor: Colors.white,),
-             ));
+              Container(
+                padding: const EdgeInsets.all(15),
+                child: TextField(
+                  controller: fullname,
+                  keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.white60,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white60, width: 3)),
+                      labelText: ' full Name',
+                      labelStyle: TextStyle(
+                        color: Colors.white60,
+                      )),
+                ),
+              ),
 
-           }
-           catch (err) {
-             print(err);
-           }
+              // ##############  Email Address Textfiled   ##############
 
+              Container(
+                padding: const EdgeInsets.all(15),
+                child: TextField(
+                  controller: email,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.white60,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white60, width: 3)),
+                      labelText: ' Email Address',
+                      labelStyle: TextStyle(
+                        color: Colors.white60,
+                      )),
+                ),
+              ),
+
+              // ##############  Password Textfiled   ##############
+
+              Container(
+                padding: const EdgeInsets.all(15),
+                child: TextField(
+                  controller: password,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.white60,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white60, width: 3)),
+                      labelText: ' Password',
+                      labelStyle: TextStyle(
+                        color: Colors.white60,
+                      )),
+                ),
+              ),
+
+              // ############## Register Button  ##############
+
+              Container(
+                  height: 50,
+                  margin: EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                    ),
+                    child: const Text('Register'),
+                    onPressed: () async {
+                      try {
+                        var authresults = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: email.text.trim(),
+                                password: password.text.trim());
+                        FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(authresults.user?.uid)
+                            .set({
+                          'username': username.text.trim(),
+                          'fullname': fullname.text.trim(),
+                          'email': email.text.trim(),
+                        }).then((value) => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignIn())));
+                      } on PlatformException catch (err) {
+                        var message =
+                            'An error occurred, Please check your credentionals';
+                        if (err.message != null) {
+                          message = err.message!;
+                        }
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                            message,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          backgroundColor: Colors.green,
+                          behavior: SnackBarBehavior.floating,
+                          action: SnackBarAction(
+                            label: 'Dismiss',
+                            onPressed: () {},
+                            textColor: Colors.white,
+                          ),
+                        ));
+                      } catch (err) {
+                        print(err);
+                      }
+                    },
+                  )),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignIn()));
                 },
-              )
-          )
-
-        ],
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-
-),
-
-
-),
     );
   }
 }
