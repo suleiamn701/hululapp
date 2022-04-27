a8ab0b4fd150dab24cf6eb9841e3e5f95897db5f
HMawali <almaawali.2014@gmail.com>
2022-04-27 05:58:30 +0400

    changing splash screen

--- lib/starter/main.dart
+++ lib/starter/main.dart
@@ -11,7 +11,7 @@ import '../models/profile_model/Snap.dart';
 import '../screens/DrawerScreen.dart';
 import '../screens/HomePage2.dart';
 
-var PROFILEIMAGE ;
+var PROFILEIMAGE;
 var NAME;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,9 +19,8 @@ void main() async {
   runApp(Hulul());
 }
 
-class  Hulul extends StatelessWidget {
+class Hulul extends StatelessWidget {
   //const Hulul({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -29,32 +28,37 @@ class  Hulul extends StatelessWidget {
     //   home: HomePage2(),
     // );
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
         home: AnimatedSplashScreen(
-      splash: Center(
-        child: Container(
-          child: Text(
-            " حلول",
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+          splash: Center(
+              child: Container(
+            child: CircleAvatar(
+              foregroundImage: AssetImage(
+                "assets/logo.png",
+              ),
+              radius: 40,
+
+              // Text(
+              //   " حلول",
+              //   style: TextStyle(
+              //       fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+              // ),
+            ),
+          )),
+          duration: 2000,
+          nextScreen: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (ctx, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                PROFILEIMAGE = NetworkImage(snapshot.data!.photoURL.toString());
+                NAME = snapshot.data!.displayName.toString();
+
+                return HomePage2(PROFILEIMAGE, NAME);
+              }
+              return SignIn();
+            },
+            //pageTransitionType: PageTransitionType.scale,
           ),
-        ),
-      ),
-      duration: 2000,
-      nextScreen: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (ctx, AsyncSnapshot snapshot) {
-
-          if (snapshot.hasData) {
-            PROFILEIMAGE =   NetworkImage(snapshot.data!.photoURL.toString());
-            NAME = snapshot.data!.displayName.toString();
-
-            return HomePage2(PROFILEIMAGE,NAME);
-          }
-          return SignIn();
-        },
-        //pageTransitionType: PageTransitionType.scale,
-      ),
-    ));
+        ));
   }
 }
